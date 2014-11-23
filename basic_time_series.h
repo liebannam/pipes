4b923d8f2ed0c37071fe29dc76f17e20b5784c5d
Anna Lieb <lieb@math.berkeley.edu>
2014-11-23 03:00:17 +0000

    errr forgot a .h file

---
+++ basic_time_series.h
@@ -0,0 +1,64 @@
+#ifndef BASIC_TIME_SERIES_H
+#define BASIC_TIME_SERIES_H
+
+void getTimeSeries(vector<Real> & bvals, vector<Real> &x, int m, int M, double T, int Fourier)
+       	
+//bvals is time series of M+1 values at t =0, T/M, ...T
+//x is a length m vector. either contains Fourier modes (Fourier==1) or of Hermite spline components (Fourier ==0)
+//we have m/2 values in Fourier series or m/2 spline interpolation points. Interpolation dt = 1/(m/2-1)
+//
+{
+	if(Fourier)	//Discrete Fourier modes:
+	//x[k] = a_k (k<=m/2)
+	//x[k] = b_j (where j =k-m/2, for k>m/2)
+	//      x =     [a_0, a_1, ...a_m/2, b_1,   b2   ...b_(m/2-1)] 
+	//                |     |       |     |     |
+	//index, k        0     1      m/2  m/2+1  m/2+2       m-1     
+	{
+		double t;
+		for(int nn = 0; nn<M+1; nn++)
+		{
+			bvals[nn] = 0.5*x[0];
+			t = (double)(nn)/(double)M;
+			for (int k = 1; k<m/2; k++)
+			{
+				bvals[nn] += x[k]*cos(2.*PI*(double)k*t) + x[k+m/2]*sin(2.*PI*(double)k*t);
+		//		cout<<k<< " "<< x[k+m/2+1]<<endl;
+			}
+			bvals[nn] +=0.5*x[m/2]*cos(PI*(double)m*t);
+		//	printf("n = %d, t/T = %f, gah %f\n", n, t, bvals[n]);
+		//	cout<<"!!!!!"<<bvals[n]<<endl;
+		}
+	}
+	else//Hermite Spline
+	//Hermite spline evaluation, adapted from C. Rycroft's code. 
+	// Evaluates spline x at point t, where
+	// x = [f(t0), f'(t0), f(t1), f'(t1), ...f(t_m), f'(tm)]
+	//      |       |        |      |          |       |
+	// i=   0       1        2      3          2*m    2*m+1
+	// x0 = 0, xm = T
+
+	{
+		double t, Dt,dt;
+		dt = T/M;//time series dt
+		Dt = T/((double)m/2.-1.);//spline dt. Careful! This is NOT the dt in your problem!
+		for(int nn = 0; nn<M+1; nn++)
+		{
+			t= nn*dt;
+			int j = int((t)/Dt);
+			t = t/Dt-j; 
+		      // cout<<Dt<<endl;	
+			if(j>m+1 || j<0) printf("warning! t=%f out of range!!, Dt = %f, T = %d\n",t,Dt,T);
+			// Calculate square and cube, and pointer to the values to use
+			double t2=t*t,t3=t2*t;
+			// Calculate the value of the spline function
+			if(j==m){bvals[nn] = x[2*j];}
+			else{bvals[nn] = x[2*j]*(2*t3-3*t2+1)+x[2*j+1]*(t3-2*t2+t)*Dt+x[2*j+2]*(-2*t3+3*t2)+x[2*j+3]*(t3-t2)*Dt;}
+		}
+	}
+}
+
+
+
+#endif
+
